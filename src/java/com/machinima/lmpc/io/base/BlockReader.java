1c2c866ae85b07fb68c096b34623de401abf8fa7
Uwe Girlich <girlich@users.sourceforge.net>
2001-05-13 19:26:32 +0000

    new files for generic Block input/output

---
+++ src/java/com/machinima/lmpc/io/base/BlockReader.java
@@ -0,0 +1,21 @@
+// $Id$
+// generic class for block reading
+
+package com.machinima.lmpc.io.base;
+import java.io.*;
+import com.machinima.lmpc.io.node.Node;
+
+public class BlockReader {
+
+
+	public BlockReader(Reader r) {
+		// no nothing
+	}
+
+	Node ReadBlock() {
+		// do nothing
+		return null;
+	}
+
+}
+
---
+++ src/java/com/machinima/lmpc/io/base/BlockWriter.java
@@ -0,0 +1,21 @@
+// $Id$
+// generic class for block writing
+
+package com.machinima.lmpc.io.base;
+import java.io.*;
+import com.machinima.lmpc.io.node.Node;
+
+public class BlockWriter {
+
+
+	public BlockWriter(Writer w) {
+		// no nothing
+	}
+
+	Node WriteBlock() {
+		// do nothing
+		return null;
+	}
+
+}
+
