3bd1089be7cdec6dcf9765d8865fb68ec9f97ce7
Uwe Girlich <girlich@users.sourceforge.net>
2001-05-20 14:58:01 +0000

    float to string conversion out-sourced

--- src/java/com/machinima/lmpc/io/node/FloatNode.java
+++ src/java/com/machinima/lmpc/io/node/FloatNode.java
@@ -2,6 +2,7 @@
 
 package com.machinima.lmpc.io.node;
 import com.machinima.lmpc.io.node.Node;
+import com.machinima.lmpc.io.base.Util;
 import java.lang.*;
 import java.text.*;
 import java.util.*;
@@ -16,30 +17,7 @@ public class FloatNode extends Node {
 
 	public String toString()
 	{
-		StringBuffer text = new StringBuffer();
-		int maxlength = 10;
-		if (value < 0.0) {
-			text.append("-");
-			maxlength++;
-		}
-		
-		float positiveFloatValue = Math.abs(value);
-		int positiveIntValue = (int)positiveFloatValue;
-		text.append(positiveIntValue);
-		if (value != 0.0 && positiveIntValue == 0) {
-			maxlength++;
-		}
-
-		text.append('.');
-
-		positiveFloatValue -= (int)positiveFloatValue;
-		while (text.length()<maxlength) {
-			positiveFloatValue *= 10.0;
-			text.append((char)('0' + (int)(positiveFloatValue) % 10));
-
-		}
-
-		return new String(text);
+		return Util.toString(10, value);
 	}
 
 }
--- src/java/com/machinima/lmpc/io/node/TimeNode.java
+++ src/java/com/machinima/lmpc/io/node/TimeNode.java
@@ -1,7 +1,10 @@
 /* $Id$ */
 
 package com.machinima.lmpc.io.node;
+
 import com.machinima.lmpc.io.node.Node;
+import com.machinima.lmpc.io.base.Util;
+
 
 public class TimeNode extends Node {
 	float value;
@@ -37,32 +40,24 @@ public class TimeNode extends Node {
 		int hour = (int)(value / 3600.0);
 		int min = (int)((value - hour * 3600.0) / 60.0);
 		double sec = value - hour * 3600.0 - min * 60.0;
-		int secFull = (int)sec;
-		int sec100 = (int)((sec-secFull)*100.0);
 
 		if (hour != 0) {
 			result.append(Integer.toString(hour));
 			result.append(':');
 			result.append(Integer.toString(min));
 			result.append(':');
-			result.append(Integer.toString(secFull));
-			result.append('.');
-			result.append(Integer.toString(sec100));
+			result.append(Util.toString(10, sec));
 			result.append('h');
 		}
 		else {
 			if (min != 0) {
 				result.append(Integer.toString(min));
 				result.append(':');
-				result.append(Integer.toString(secFull));
-				result.append('.');
-				result.append(Integer.toString(sec100));
+				result.append(Util.toString(10, sec));
 				result.append('m');
 			}
 			else {
-				result.append(Integer.toString(secFull));
-				result.append('.');
-				result.append(Integer.toString(sec100));
+				result.append(Util.toString(10, sec));
 				result.append('s');
 			}
 		}
