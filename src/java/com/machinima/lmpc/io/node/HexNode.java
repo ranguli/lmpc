849254ceb371f8ba056330b660f35db763622514
Uwe Girlich <girlich@users.sourceforge.net>
2001-06-13 18:06:12 +0000

    hex digits > 9 were totally wrong

--- src/java/com/machinima/lmpc/io/node/HexNode.java
+++ src/java/com/machinima/lmpc/io/node/HexNode.java
@@ -13,9 +13,10 @@ public class HexNode extends IntNode {
 	public String toString()
 	{
 		StringBuffer buffer = new StringBuffer("0x");
-		// We allow only byte values. Maybe we should check this here ?
-		buffer.append((char)(((value & 0xf0)>>4) + '0'));
-		buffer.append((char)(((value & 0x0f)   ) + '0'));
+		int digit1 = ((value & 0xf0)>>>4);
+		buffer.append( (char)((digit1<10 ? '0' : 'A'-10) + digit1) );
+		int digit2 = ((value & 0x0f)    );
+		buffer.append( (char)((digit2<10 ? '0' : 'A'-10) + digit2) );
 		return buffer.toString();
 	}
 }
