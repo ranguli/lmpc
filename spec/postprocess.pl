0696ca57b5a40f3bd25e72804f8c2fc943395c47
Uwe Girlich <girlich@users.sourceforge.net>
2004-05-20 17:30:53 +0000

    split the post processing in twho parts: one post linuxdoc->docbook30 and one post sgmlnorm make all tags lowercase, as the DocBook XML specs expects it so.

--- spec/postprocess.pl
+++ spec/postprocess.pl
@@ -6,19 +6,41 @@
 
 use strict;
 
+my $step = shift;
+
 $/='';
 my $text = '';
 while (<>) { $text .= $_; }
 
-$text =~ s,-//Davenport//DTD DocBook V3.0//EN,-//OASIS//DTD DocBook V4.2//EN,;
-$text =~ s,ArtHeader,ArticleInfo,g;
-$text =~ s,&#38;,&amp;,g;
-$text =~ s,&#60;,&lt;,g;
-$text =~ s,&#62;,&gt;,g;
-$text =~ s,&#65533;,,g;
-$text =~ s:<FirstName>\@MYNAME@, </FirstName>:<FIRSTNAME>\@MYFIRSTNAME@</FIRSTNAME><SURNAME>\@MYSURNAME@</SURNAME>:;
-$text =~ s:</AuthorBlurb>:</AuthorBlurb><affiliation><address><email>\@MYEMAIL@</email></address></affiliation>:;
-$text =~ s:<AuthorBlurb>.*</AuthorBlurb>::s;
+if ($step == "1") {
+	$text =~ s,-//Davenport//DTD DocBook V3.0//EN,-//OASIS//DTD DocBook V4.2//EN,;
+	$text =~ s,ArtHeader,ArticleInfo,g;
+}
+
+if ($step == "2") {
+
+	$text =~ s,&#38;,&amp;,g;
+	$text =~ s,&#60;,&lt;,g;
+	$text =~ s,&#62;,&gt;,g;
+	$text =~ s,&#65533;,,g;
+	$text =~ s:<FirstName>\@MYNAME@, </FirstName>:<FirstName>\@MYFIRSTNAME@</FirstName><SurName>\@MYSURNAME@</SurName>:i;
+	$text =~ s:(</AuthorBlurb>):$1<Affiliation><Address><Email>\@MYEMAIL@</Email></Address></Affiliation>:i;
+	$text =~ s:<AuthorBlurb>.*</AuthorBlurb>::si;
+	$text =~ s:(<ColSpec[^>/]+)>:$1/>:gi;
+	$text =~ s:(<XRef[^>/]+)>:$1/>:gi;
+	# We need everything as lowercase.
+	# SGML does not care and XML needs it so.
+	$text =~ s:(<[^>]+>):lc($1):ge;
+
+	print '<!ENTITY lowbar "_">
+	<!ENTITY lsqb "[">
+	<!ENTITY rsqb "]">
+	<!ENTITY num "#">
+	<!ENTITY bsol "\">
+	]>';
+
+}
 
 print $text;
+
 
